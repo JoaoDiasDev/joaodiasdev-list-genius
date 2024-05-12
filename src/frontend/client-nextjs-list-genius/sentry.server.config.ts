be410d228c3aa0f802496fe6964a76df3610906b
JoaoDias <joaodiasworking@gmail.com>
2024-05-12 17:13:23 -0300

    fix: Adjusting sentry config and adding pt-BR locale file.

--- src/frontend/client-nextjs-list-genius/sentry.server.config.ts
+++ src/frontend/client-nextjs-list-genius/sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: 'https://0fd04a7130e7685dec54c55c7ea94abc@o836751.ingest.us.sentry.io/4507245049741312'
+  dsn: 'https://0fd04a7130e7685dec54c55c7ea94abc@o836751.ingest.us.sentry.io/4507245049741312',
 
   // Enable Spotlight in development
   spotlight: process.env.NODE_ENV === 'development',
