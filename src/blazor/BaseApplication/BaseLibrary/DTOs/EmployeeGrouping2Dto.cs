f8556d42b7a1f8567a6ac3a74992a469db8c4eee
JoaoDias <joaodiasworking@gmail.com>
2024-06-23 13:54:15 -0300

    feat: Almost done with AddOrUpdateEmployee Component on BaseApplication.

---
+++ src/blazor/BaseApplication/BaseLibrary/DTOs/EmployeeGrouping2Dto.cs
@@ -0,0 +1,10 @@
+﻿namespace BaseLibrary.DTOs;
+
+public class EmployeeGrouping2Dto
+{
+    [Required] public string JobName { get; set; } = string.Empty;
+    [Required, Range(1, 99999, ErrorMessage = "You must select a branch")] public int BranchId { get; set; }
+    [Required, Range(1, 99999, ErrorMessage = "You must select a town")] public int TownId { get; set; }
+    public string? Other { get; set; }
+
+}
